ed89c16efdf192ffc13fbf8b9990ffbf6a7f8a47
Andy-Guzman <andy.guzman.970@alu.tecnica29de6.edu.ar>
2026-04-27 11:44:09 -0300

    Update build.gradle.kts comments to Spanish

--- Codigo/build.gradle.kts
+++ Codigo/build.gradle.kts
@@ -1,4 +1,4 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Archivo de gran autoridad donde se puede añadir opciones de configuracion comunes para todos los modulos o sub proyectos.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
